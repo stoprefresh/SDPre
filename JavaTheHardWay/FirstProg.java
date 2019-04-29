bceff14d6940a65cc1f4ed96e48849c2d12dbf61
Miguel <marsigliamiguel@protonmail.com>
2019-04-29 16:12:35 -0600

    Completed jthw ex 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main(String[] args) {
-    System.out.println("I am determined to learn how to code. ");
-    System.out.println("Today's date is");
+    System.out.println("I, Miguel Marsiglia, am determined to learn how to code. ");
+    System.out.println("Today's date is Monday, April 29th, 2019");
   }
 }
