379f61c6f7dbd22fb04a6e6a1b410008e27e660f
Miguel <marsigliamiguel@protonmail.com>
2019-04-29 17:54:06 -0600

    Finished jthw ex 2

--- JavaTheHardWay/GasolineReceipt.java
+++ JavaTheHardWay/GasolineReceipt.java
@@ -2,14 +2,14 @@ public class GasolineReceipt {
   public static void main(String[] args) {
     System.out.println("+------------------------+");
     System.out.println("|                        |");
-    System.out.println("|      CORNER STORE      |");
+    System.out.println("| CORNER STORE - DENVER  |");
     System.out.println("|                        |");
-    System.out.println("| 2015-03-29  04:38PM    |");
+    System.out.println("| 2019-04-29  05:19PM    |");
     System.out.println("|                        |");
-    System.out.println("| Gallons:       10.870  |");
-    System.out.println("| Price/gallon:  $2.089  |");
+    System.out.println("| Gallons:       12.153  |");
+    System.out.println("| Price/gallon:  $2.64   |");
     System.out.println("|                        |");
-    System.out.println("| Fuel total:   $22.71   |");
+    System.out.println("| Fuel total:   $32.84   |");
     System.out.println("|                        |");
     System.out.println("+------------------------+");
   }
