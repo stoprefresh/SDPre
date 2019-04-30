817bf00691430a28d73a806bff0a698a29e97100
Miguel <marsigliamiguel@protonmail.com>
2019-04-30 09:54:53 -0600

    Completed Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,15 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "Japanese Curry";
+    food2 = "Raspberries";
+    food3 = "Crawfish Boils";
+
+    System.out.println("My Favorite Foods Are:");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
+
+  }
+}
