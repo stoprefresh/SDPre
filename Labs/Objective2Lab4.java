4da311d6cc416c36d66d1342ca7d8f691293f2c1
Miguel <marsigliamiguel@protonmail.com>
2019-04-30 10:00:33 -0600

    Completed Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,14 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 12;
+    num2 = 14;
+    num3 = 20;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+
+  }
+}
