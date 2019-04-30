29571f5acdd22b7e90fae44ba57c776a62e25996
Miguel <marsigliamiguel@protonmail.com>
2019-04-30 09:46:31 -0600

    Completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name ="Miguel Marsiglia";
+    int age = 30;
+    String state = "Minnesota";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
