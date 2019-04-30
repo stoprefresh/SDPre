393041831076f9d1c5ae53a1c2583353bd8cdf6d
Miguel <marsigliamiguel@protonmail.com>
2019-04-29 19:44:52 -0600

    Completed Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+
+    System.out.println("Name : Miguel Marsiglia");
+    System.out.println("Age : 30");
+    System.out.println("State : Minnesota");
+
+  }
+}
