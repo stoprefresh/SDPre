c3612a84ac60d4aa29369df593b195ad22eba502
Miguel <marsigliamiguel@protonmail.com>
2019-05-03 14:23:48 -0700

    Completed Objective6Lab2

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,10 @@
+public class Objective6Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    while (counter < 20) {
+      counter = counter + 1;
+      System.out.println(counter);
+    }
+  }
+}
