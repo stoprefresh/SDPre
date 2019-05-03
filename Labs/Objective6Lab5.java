b691e6deb2b524fcdb1ad98fbfa08c081b627a3c
Miguel <marsigliamiguel@protonmail.com>
2019-05-03 15:18:00 -0700

    Completed Objective6Lab5

---
+++ Labs/Objective6Lab5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Objective6Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+    selection = 0;
+
+    while (selection != 3) {
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = scanner.nextInt();
+
+      if (selection == 1) {
+        System.out.println("Hello Human");
+      }
+      else if (selection == 2){
+        System.out.println("Apples, Bananas, Coconuts");
+      }
+      else {
+        System.out.println("Goodbye");
+      }
+    }
+
+    scanner.close();
+  }
+}
