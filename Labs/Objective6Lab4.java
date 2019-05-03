9d16fe8abfe1ebf1ea24e4ad227e1c844a891970
Miguel <marsigliamiguel@protonmail.com>
2019-05-03 15:04:17 -0700

    Completed Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int totalSum, counter;
+    totalSum = 0;
+    counter = 1;
+
+    while (totalSum < 210) {
+      totalSum += counter;
+    }
+    System.out.println(totalSum);
+  }
+}
